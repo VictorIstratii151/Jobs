8c4506019e5ca6ef0a71b7a06336097db324c495
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-06-19 21:00:20 +0300

    Added to post table also company email and phone number

--- jobsApp/database/migrations/2017_06_19_084301_create_posts_table.php
+++ jobsApp/database/migrations/2017_06_19_084301_create_posts_table.php
@@ -16,6 +16,8 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('company_name');
+            $table->string('company_email', 100);
+            $table->string('company_phone_number');
             $table->string('title');
             $table->text('body');
             $table->timestamps();
