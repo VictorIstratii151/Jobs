7fdbf0bd2298abf3009ef4f1cf52d34684d71a90
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-06-22 22:07:51 +0300

    Added a view for showing a post with detailed info

---
+++ jobsApp/resources/views/posts/show.blade.php
@@ -0,0 +1,32 @@
+@extends ('layouts.master')
+
+@section ('content')
+	
+	<div class="row">
+		<div class="col-md-4">
+			<div class="title">
+				<h2>{{ $post->title }}</h2>
+				Company: {{ $post->company_name }}
+			</div>
+
+			<hr>
+
+			<div class="body">
+				<p>
+					{{ $post->body }}
+				</p>
+			</div>
+
+			<div class="info">
+				Telephone: {{ $post->company_phone_number }}
+				<hr>
+				Email: {{ $post->company_email }}
+			</div>
+
+			
+
+		</div>
+	</div>
+	
+
+@endsection
