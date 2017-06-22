6bbd94192402715e762dfdd26219cc68c0d20279
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-06-22 18:27:13 +0300

    Added to nav a button for creating a new post

--- jobsApp/resources/views/layouts/nav.blade.php
+++ jobsApp/resources/views/layouts/nav.blade.php
@@ -7,8 +7,15 @@
       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
 
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <li class="nav-item">
+          
+            <div class="form-inline my-2 my-lg-3">
+              <form action="/create">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Create an Ad</button>
+              </form>
+
+              
+            </div>
           </li>
 
           <li class="nav-item dropdown">
