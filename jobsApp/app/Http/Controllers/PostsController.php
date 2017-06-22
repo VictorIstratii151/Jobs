7e25ffd7531128460c4fb53e39cee6e24b6a0d5c
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-06-22 18:31:38 +0300

    Defined the PostController for validating the data received from form and creating a new user

--- jobsApp/app/Http/Controllers/PostsController.php
+++ jobsApp/app/Http/Controllers/PostsController.php
@@ -13,4 +13,36 @@ class PostsController extends Controller
     	$posts = Post::paginate(3);
     	return view('posts.index', compact('posts'));
     }
+
+    public function create()
+    {
+    	return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+
+        $this->validate($request, ['title' => 'required',
+                                'body' => 'required',
+                                'name' => 'required',
+                                'email' => 'required',
+                                'phone_number' => 'required']);
+
+        $data = $request->input();
+
+        Post::create([
+            'company_name' => $data['name'],
+            'company_email' => $data['email'],
+            'company_phone_number' => $data['phone_number'],
+            'title' => $data['title'],
+            'body' => $data['body']
+        ]);
+
+        $responseData = [
+            'success' => true,
+            'message' => 'Your AJAX processed correctly'
+        ];
+
+        return response()->json($responseData, 200, [], JSON_PRETTY_PRINT);
+    }
 }
